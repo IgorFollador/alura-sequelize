fdf1b23527811818f0c17deb96f6f276eed8c373
Igor Follador <igorledf@hotmail.com>
2022-05-06 21:37:07 -0300

    Create model and migration to Pessoas

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-const port = 3000
+const port = 3000;
 
 app.get("/teste", (req, res) => {
     res.status(200)
         .send({ messagem: 'boas-vinda à API' })
-})
+});
 
-app.listen(port, () => console.log(`Sevidor está rodando na porta ${port}`))
+app.listen(port, () => console.log(`Sevidor está rodando na porta ${port}`));
 
-module.exports = app
+module.exports = app;
