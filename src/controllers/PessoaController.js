65a45255e323cfbed5a95eb8c2f4de4647d4195c
IgorFollador <igorledf@hotmail.com>
2022-05-10 21:02:02 -0300

    Read Pessoa by ID

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -9,6 +9,20 @@ class PessoaController {
             return res.status(500).json(error.message);
         }
     }
+
+    static async readPessoaById(req, res) {
+        const { id } = req.params;
+        try {
+            const pessoa = await database.Pessoas.findOne({ 
+                where: { 
+                    id: Number(id) 
+                } 
+            });
+            return res.status(200).json(pessoa);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
 }
 
 module.exports = PessoaController
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.readAllPessoas);
+router.get('/pessoas/:id', PessoaController.readPessoaById);
 
 module.exports = router;
